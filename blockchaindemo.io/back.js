57e70f06024229f5b2f3bcb78c120c353c3a34e4
Rolando Mata <rdma95@gmail.com>
2020-03-11 12:37:26 -0600

    Main rename/added Backend file

---
+++ blockchaindemo.io/back.js
@@ -0,0 +1,9 @@
+// En este archivo generaremos el backend de la pagina
+/* function flipCard(){
+  var bloque = main.getElementById("bloque-presente");
+  var adoptButton= main.getElementById("adopt-button");
+    adoptButton.click(function(){
+      bloque.toggleClass("flip");
+      alert("jajas");
+    });
+}; */
