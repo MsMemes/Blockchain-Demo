4b0a72132c1b9954d5e15b2f133171bf8b3bbbfd
sergon <sgzzsifuentes@gmail.com>
2020-03-12 16:20:13 -0600

    Solved profile 4 discrepancy

--- blockchaindemo.io/back.js
+++ blockchaindemo.io/back.js
@@ -1,3 +1,30 @@
+var a = true;
+
+
+function addimage() { 
+ if (a == true){
+   var img = document.createElement("img");
+   img.src = "perfil.png"; 
+   //optionally set a css class on the image
+   var class_name = "perfil_3";
+   img.setAttribute("class", class_name);
+  document.getElementById("Group_1_y").appendChild(img);
+
+ }
+
+if (a == false){
+
+var img = document.createElement("img");
+img.src = "perfil4.png"; 
+//optionally set a css class on the image
+var class_name = "perfil_4";
+img.setAttribute("class", class_name);
+document.getElementById("Group_1_y").appendChild(img);
+
+}
+a = false;
+}
+
 function stringToHash(string)
 {
   var hash = 0;
@@ -46,4 +73,4 @@ document.getElementById("prevBPHash").innerHTML = gBlock.prevHash;
 document.getElementById("prevBHash").innerHTML = gBlock.hash;
 
 // Right block is hidden, to display change this attribute to "block"
-document.getElementById("bloque-presente").style.display = "none";
+// document.getElementById("bloque-presente").style.display = "none";
